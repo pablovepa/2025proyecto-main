d1090a6b39be167740935877d1bd2db0bde34a4e
pablo vega <pablovega6756@gmail.com~>
2025-06-03 17:47:16 -0300

    arreglos de codigo

--- backend/mockups/user.js
+++ backend/mockups/user.js
@@ -3,21 +3,21 @@ import { hash } from "bcrypt";
 export class UserMockup { 
  static users = [ 
 {
-   id : 1,
-username: `admin` ,
+      id : 1,
+   username: `admin` ,
 
-hashedPassword: "$2b$10$Qi9r/C8WheuQFPWpvqbKNuPJOSP2ngNx6DsQO8uQfhJ72HDirX3s.",
-name: `admin` ,
-email: `admin@fae.com` ,
-roles: [`admin`], 
+   hashedPassword: "$2b$10$Qi9r/C8WheuQFPWpvqbKNuPJOSP2ngNx6DsQO8uQfhJ72HDirX3s.",
+   name: `admin` ,
+   email: `admin@fae.com` ,
+   roles: [`admin`], 
  },
 { 
-id : 2,
-username: `admin2` ,
-password: `1234` ,
-name: `admin2` ,
-email: `admin2@fae.com` , 
-roles: [`admin2`],   } 
+   id : 2,
+   username: `admin2` ,
+   password: `1234` ,
+   name: `admin2` ,
+   email: `admin2@fae.com` , 
+   roles: [`admin2`],   } 
 ]
 
  static async getSignleOrNullByUsername (username) {
